8be00ab84a04fd5047236f517500bd852a6a808d
Wasawat Pruthipunyaskul <wasawatp@NBAAP2100004.local>
2022-08-06 17:40:10 +0700

    feat(command): add version sub command

---
+++ cmd/rdc/version.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+const Major = "0"
+const Minor = "1"
+const Fix = "0"
+const Verbal = "TX Add && Balances List"
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Describes version.",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("Verison: %s.%s.%s-beta %s", Major, Minor, Fix, Verbal)
+	},
+}
+
+func init() {
+	rdcCmd.AddCommand(versionCmd)
+}
