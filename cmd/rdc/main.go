56532aae1816bf13da53a3380ff345b7e00d220c
Wasawat Pruthipunyaskul <wasawatp@NBAAP2100004.local>
2022-08-06 19:14:36 +0700

    feat(command): add sub command

--- cmd/rdc/main.go
+++ cmd/rdc/main.go
@@ -7,18 +7,27 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var rdcCmd = &cobra.Command{
-	Use:   "rdc",
-	Short: "The Roodic Blockchain CLI",
-	Run: func(cmd *cobra.Command, args []string) {
-	},
-}
-
 func main() {
+
+	var rdcCmd = &cobra.Command{
+		Use:   "rdc",
+		Short: "The Roodic Blockchain CLI",
+		Run: func(cmd *cobra.Command, args []string) {
+		},
+	}
+
+	rdcCmd.AddCommand(versionCmd)
+	rdcCmd.AddCommand(balanceCmd())
+	rdcCmd.AddCommand(txCmd())
 
 	err := rdcCmd.Execute()
 	if err != nil {
 		fmt.Println(os.Stderr, err)
 		os.Exit(1)
 	}
+
+}
+
+func incorrectUsageErr() error {
+	return fmt.Errorf("incorrect usage")
 }
